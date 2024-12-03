fa0c60dad99068c17b517edf3bbcbdcb1262b050
Gitfordevta <shivamjha.vj16@gmail.com>
2024-12-03 19:58:10 +0530

    first commit

---
+++ src/app/tenants/tenants.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-tenants',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatCheckboxModule,
+  ],
+  templateUrl: './tenants.component.html',
+  styleUrl: './tenants.component.scss',
+})
+export class TenantsComponent {}
