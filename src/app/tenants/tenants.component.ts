16c7ae9f374a0f127c0932d60ac0754645a92400
Gitfordevta <shivamjha.vj16@gmail.com>
2024-12-03 22:35:15 +0530

    add input file and validation

--- src/app/tenants/tenants.component.ts
+++ src/app/tenants/tenants.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -7,6 +14,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
+import { NgxMatFileInputModule } from '@angular-material-components/file-input';
+import { ThemePalette } from '@angular/material/core';
+
+interface Tenant {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-tenants',
@@ -21,8 +35,68 @@ import { MatSelectModule } from '@angular/material/select';
     MatCardModule,
     MatInputModule,
     MatCheckboxModule,
+    NgxMatFileInputModule,
   ],
   templateUrl: './tenants.component.html',
   styleUrl: './tenants.component.scss',
 })
-export class TenantsComponent {}
+export class TenantsComponent {
+  tenantForm: FormGroup;
+
+  tenantTypeOptions: Tenant[] = [
+    { value: '', viewValue: '--Select--' },
+    { value: 'family', viewValue: 'Family' },
+    { value: 'bachelor', viewValue: 'Bachelor' },
+  ];
+  selectedType = this.tenantTypeOptions[0].value;
+
+  floor: Tenant[] = [
+    { value: '', viewValue: '--Select--' },
+    { value: 'ground-left', viewValue: 'Ground Left' },
+    { value: 'ground-right', viewValue: 'Ground Right' },
+    { value: 'first-left', viewValue: 'First left' },
+    { value: 'first-right', viewValue: 'First Right' },
+    { value: 'second-left', viewValue: 'Second left' },
+    { value: 'second-right', viewValue: 'Second Right' },
+  ];
+  selectedFloor = this.floor[0].value;
+
+  color: ThemePalette = 'primary';
+  disabled: boolean = false;
+  multiple: boolean = false;
+  accept: string;
+
+  fileControl: FormControl;
+
+  //validations
+  constructor(private fb: FormBuilder) {
+    this.tenantForm = this.fb.group({
+      name: ['', Validators.required],
+      tenantType: ['', Validators.required],
+      mobile: [
+        '',
+        [Validators.required, Validators.pattern('^[6-9]\\d{9}$')], // Indian mobile number pattern
+      ],
+    });
+  }
+
+  get name() {
+    return this.tenantForm.get('name');
+  }
+
+  get tenantType() {
+    return this.tenantForm.get('tenantType');
+  }
+
+  get mobile() {
+    return this.tenantForm.get('mobile');
+  }
+
+  onSubmit() {
+    if (this.tenantForm.valid) {
+      console.log('Form Submitted:', this.tenantForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
